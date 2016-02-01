8b1a7392ab2242ca56640f6ff06b7e851c0a7bbf
bdooley@redhat.com <bdooley@redhat.com>
2016-02-01 15:57:44 +0000

    update file /FHStarterProject/FHStarterProjectViewController.m

--- FHStarterProject/FHStarterProjectViewController.m
+++ FHStarterProject/FHStarterProjectViewController.m
@@ -5,7 +5,6 @@
 //
 
 #import "FHStarterProjectViewController.h"
-#import "Reachability.h"
 #import <SystemConfiguration/SystemConfiguration.h>
 
 @interface FHStarterProjectViewController ()
@@ -17,7 +16,7 @@
 @synthesize button, name, result;
 
 - (void)viewDidLoad {
-  [super viewDidLoad];
+    [super viewDidLoad];
 }
 
 - (IBAction)cloudCall:(id)sender {
@@ -27,39 +26,45 @@
         NSDictionary *args = [NSDictionary dictionaryWithObject:name.text forKey:@"hello"];
         FHCloudRequest *req = (FHCloudRequest *) [FH buildCloudRequest:@"/hello" WithMethod:@"POST" AndHeaders:nil AndArgs:args];
 
-        if (![FH isOnline]) { {
+        
+        
+        if (![FH isOnline]) {
             // not connected
-            UIAlertController *alertController = [UIAlertController
-                                                  alertControllerWithTitle: @"Alert"
-                                                  message: @"not connected"
-                                                  preferredStyle:UIAlertControllerStyleAlert];
-
-            [self presentViewController:alertController animated:YES completion:nil];
+            UIAlertView *alertView = [[UIAlertView alloc]
+                                      initWithTitle:@"DefaultStyle"
+                                      message:@"not connected"
+                                      delegate:self
+                                      cancelButtonTitle:@"Cancel"
+                                      otherButtonTitles:@"OK", nil];
+            
+            [alertView show];
         } else {
             // not connected.
-            UIAlertController *alertController = [UIAlertController
-                                                  alertControllerWithTitle: @"Alert"
-                                                  message: @"connected"
-                                                  preferredStyle:UIAlertControllerStyleAlert];
+            UIAlertView *alertView = [[UIAlertView alloc]
+                                      initWithTitle:@"DefaultStyle"
+                                      message:@"connected"
+                                      delegate:self
+                                      cancelButtonTitle:@"Cancel"
+                                      otherButtonTitles:@"OK", nil];
             
-            [self presentViewController:alertController animated:YES completion:nil];
+            [alertView show];
         }
-
-        [req execAsyncWithSuccess:^(FHResponse * res) {
-            // Response
-            NSLog(@"Response: %@", res.rawResponseAsString);
-            result.text = [res.parsedResponse objectForKey:@"msg"];
-        } AndFailure:^(FHResponse * res){
-            // Errors
-            NSLog(@"Failed to call. Response = %@", res.rawResponseAsString);
-            result.text = res.rawResponseAsString;
+            
+            [req execAsyncWithSuccess:^(FHResponse * res) {
+                // Response
+                NSLog(@"Response: %@", res.rawResponseAsString);
+                result.text = [res.parsedResponse objectForKey:@"msg"];
+            } AndFailure:^(FHResponse * res){
+                // Errors
+                NSLog(@"Failed to call. Response = %@", res.rawResponseAsString);
+                result.text = res.rawResponseAsString;
+            }];
+            
+        } AndFailure:^(FHResponse *response) {
+            NSLog(@"initialize fail, %@", response.rawResponseAsString);
         }];
-        
-    } AndFailure:^(FHResponse *response) {
-        NSLog(@"initialize fail, %@", response.rawResponseAsString);
-    }];
-}
-
-
-@end
-
+    }
+     
+     
+     @end
+     
