2e82b8292eb8bd158cb47c99c53b74b2e8cdd7b9
bdooley@redhat.com <bdooley@redhat.com>
2016-02-01 16:28:42 +0000

    update file /FHStarterProject/FHStarterProjectViewController.m

--- FHStarterProject/FHStarterProjectViewController.m
+++ FHStarterProject/FHStarterProjectViewController.m
@@ -22,11 +22,7 @@
 - (IBAction)cloudCall:(id)sender {
     [FH initWithSuccess:^(FHResponse *response) {
         NSLog(@"initialized OK");
-        
-        NSDictionary *args = [NSDictionary dictionaryWithObject:name.text forKey:@"hello"];
-        FHCloudRequest *req = (FHCloudRequest *) [FH buildCloudRequest:@"/hello" WithMethod:@"POST" AndHeaders:nil AndArgs:args];
-        
-        
+
         NSLog(@"performing connection check");
         if (![FH isOnline]) {
             // not connected
@@ -35,16 +31,19 @@
             NSLog(@"connected");
         }
         NSLog(@"finished connection check");
-            
-            [req execAsyncWithSuccess:^(FHResponse * res) {
-                // Response
-                NSLog(@"Response: %@", res.rawResponseAsString);
-                result.text = [res.parsedResponse objectForKey:@"msg"];
-            } AndFailure:^(FHResponse * res){
-                // Errors
-                NSLog(@"Failed to call. Response = %@", res.rawResponseAsString);
-                result.text = res.rawResponseAsString;
-            }];
+        
+        NSDictionary *args = [NSDictionary dictionaryWithObject:name.text forKey:@"hello"];
+        FHCloudRequest *req = (FHCloudRequest *) [FH buildCloudRequest:@"/hello" WithMethod:@"POST" AndHeaders:nil AndArgs:args];
+
+          [req execAsyncWithSuccess:^(FHResponse * res) {
+              // Response
+              NSLog(@"Response: %@", res.rawResponseAsString);
+              result.text = [res.parsedResponse objectForKey:@"msg"];
+          } AndFailure:^(FHResponse * res){
+              // Errors
+              NSLog(@"Failed to call. Response = %@", res.rawResponseAsString);
+              result.text = res.rawResponseAsString;
+          }];
             
         } AndFailure:^(FHResponse *response) {
             NSLog(@"initialize fail, %@", response.rawResponseAsString);
