61faf01f222c923b847719311398e1e2daea138b
tomcat6 <tomcat6@tom1-core-app2>
2016-02-01 14:28:56 +0000

    Initialize app from template

---
+++ FHStarterProject/FHStarterProjectViewController.m
@@ -0,0 +1,46 @@
+//
+//  HomeViewController.m
+//  iOS-Template-App
+//
+//
+
+#import "FHStarterProjectViewController.h"
+
+@interface FHStarterProjectViewController ()
+
+@end
+
+@implementation FHStarterProjectViewController
+
+@synthesize button, name, result;
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+}
+
+
+- (IBAction)cloudCall:(id)sender {
+    [FH initWithSuccess:^(FHResponse *response) {
+        NSLog(@"initialized OK");
+        
+        NSDictionary *args = [NSDictionary dictionaryWithObject:name.text forKey:@"hello"];
+        FHCloudRequest *req = (FHCloudRequest *) [FH buildCloudRequest:@"/hello" WithMethod:@"POST" AndHeaders:nil AndArgs:args];
+        
+        [req execAsyncWithSuccess:^(FHResponse * res) {
+            // Response
+            NSLog(@"Response: %@", res.rawResponseAsString);
+            result.text = [res.parsedResponse objectForKey:@"msg"];
+        } AndFailure:^(FHResponse * res){
+            // Errors
+            NSLog(@"Failed to call. Response = %@", res.rawResponseAsString);
+            result.text = res.rawResponseAsString;
+        }];
+        
+    } AndFailure:^(FHResponse *response) {
+        NSLog(@"initialize fail, %@", response.rawResponseAsString);
+    }];
+}
+
+
+@end
+
